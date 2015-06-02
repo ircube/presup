f0d8317f83ac35ec7918816bd7c18fab3e622b76
Ivo <ivo753@gmail.com>
2015-06-02 15:23:05 -0400

    Update Constants.java
    added web cliend id

--- src/main/java/com/presup/Constants.java
+++ src/main/java/com/presup/Constants.java
@@ -5,8 +5,9 @@ import com.google.api.server.spi.Constant;
 /**
  * Contains the client IDs and scopes for allowed clients consuming the conference API.
  */
+
 public class Constants {
-    public static final String WEB_CLIENT_ID = "replace this with your Web cliend ID";
+    public static final String WEB_CLIENT_ID = "271955799872-l5dpm57b4e20ab659uj6qocbii9g85ul.apps.googleusercontent.com";
     public static final String ANDROID_CLIENT_ID = "replace this with your Android client ID";
     public static final String IOS_CLIENT_ID = "replace this with your iOS client ID";
     public static final String ANDROID_AUDIENCE = WEB_CLIENT_ID;
